ea5d10ccd132986e3fe42e862c408bc38c86d1b6
John Christy <jchrist3@citadel.edu>
2017-09-06 03:34:50 +0000

    mostly finished hw_intro

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,81 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  x = 0
+  arr.each { |a| x += a } if arr.all? { |i| i.is_a?(Integer)}
+  return x
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  x = 0
+  y = 0
+  if arr.all? { |i| i.is_a?(Integer)} && arr.length > 0
+    arr.sort! { |a, b| b <=> a }
+    x = arr[0]
+     if arr.length > 1 
+       y = arr[1]
+      end
+  end
+  return x + y
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  !!arr.combination(2).detect { |a, b| a + b == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  c = s.downcase.split(//)
+  v = ['a', 'e', 'i', 'o', 'u']
+  f = ('a'..'z').to_a
+  !v.detect { |a| a == c[0] or !f.detect { |l| l == c[0]}}
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  b = s.split(//)
+  if b.all? { |n| n == 0 or n == 1 }
+    if (b[-1] == 0 and b[-2] == 0)
+      return true
+    else
+      return false
+    end
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    @isbn = isbn
+    @p = price
+  end
+  
+  def ArgumentError
+    assert raises ArgumentError do
+      BookInStock.new("", <= 0)
+    end
+    
+    def isbn
+      @isbn
+    end
+    
+    def isbn(val)
+      isbn = val
+    end
+    
+    def price
+      @price
+    end
+    
+    def price(val)
+      price = val
+    end
+    
 end
